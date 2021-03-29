5c262fa703b9d7eba3bf0aa54aa87999c8837f9d
Sava Hristov <i400752@fhict.nl>
2021-03-29 20:22:27 +0300

    Added 1st level, player movement and shooting system

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject impactEffect;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            Instantiate(impactEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.tag == "Dummy")
+        {
+            Instantiate(impactEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+        
+    }
+}
