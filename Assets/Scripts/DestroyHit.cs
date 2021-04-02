07c3de73d5118f3d799870e5a0b134fe5ca362e6
Sava Hristov <i400752@fhict.nl>
2021-04-02 17:53:01 +0300

    Bug fix

---
+++ Assets/Scripts/DestroyHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float destructionTimer = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destructionTimer -= Time.deltaTime;
+        if (destructionTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
