089f08644adbd822be7f945eb14bb798c7bb41da
Alexander Tsvetkov <i428291@fhict.nl>
2021-04-06 14:29:31 +0200

    TalentTreeUI v1

---
+++ Assets/TalentTreeOpen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalentTreeOpen : MonoBehaviour
+{
+    public Canvas TalentTree;
+    bool isOpened = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TalentTree.enabled = false;
+    }
+
+    public void ShowTalentTree()
+    {
+        TalentTree.enabled = true;
+    }
+
+    public void HideTalentTree()
+    {
+        TalentTree.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if(isOpened == false)
+            {
+                TalentTree.enabled = true;
+                isOpened = true;
+            }
+            else
+            {
+                TalentTree.enabled = false;
+                isOpened = false;
+            }
+        }
+
+                    
+    }
+}
---
+++ Assets/TalentTreeUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TalentTreeUpgrade : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image HealthPath1;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
