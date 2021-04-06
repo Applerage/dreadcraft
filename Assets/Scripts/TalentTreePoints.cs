75375d2292db734f397c5d0aea76f37548eee0b9
Sava Hristov <i400752@fhict.nl>
2021-04-06 16:07:30 +0300

    Minor changes idk

---
+++ Assets/Scripts/TalentTreePoints.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalentTreePoints : MonoBehaviour
+{
+    private PlayerResources pr;
+
+    public Text talentPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pr = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerResources>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        talentPoints.text = $"Talent Points: {pr.currentTalentPoints}";
+    }
+}
