b22e302d998d387d3217c0fac8c9f7da70d19535
Sava Hristov <i400752@fhict.nl>
2021-04-09 02:03:36 +0300

    Created world + made enemies a bit harder

---
+++ Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    private PlayerResources pr;
+    
+    private void Start()
+    {
+        pr = GetComponent<PlayerResources>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            pr.stamina += collision.gameObject.GetComponent<ItemStats>().stamina;
+            pr.intellect += collision.gameObject.GetComponent<ItemStats>().intellect;
+            pr.onItemCollection();
+            Destroy(collision.gameObject);
+        }
+    }
+}
